e18e67aff83e40d5539efa1633fc08facea36121
Mike Dilley <mike.dilley@gmail.com>
2018-10-19 16:46:13 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,20 @@
+class Artist
+
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    add_song(song_name)
+    song.artist = self
+  end
+
+end
